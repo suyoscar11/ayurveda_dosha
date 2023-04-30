636ecff6f4e0cafba504032ed6f3b2aaf52d5fe6
Suyoscar11 <suyoscar11@gmail.com>
2023-04-30 16:09:59 +0545

    added border radius and gave some color to it

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,6 +10,22 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ayurveda Dosha Test'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            width: MediaQuery.of(context).size.width,
+            child: Text(
+                ' Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiamolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborummquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumtio, eaque rerum! Provident similique accus'),
+          )
+        ],
+      ),
+    );
   }
 }
