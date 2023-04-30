f92ebf58109692816c1173803b2220a2a96f1396
Suyoscar11 <suyoscar11@gmail.com>
2023-04-30 13:50:45 +0545

    questions model✅ started UI

---
+++ android/app/src/main/kotlin/com/example/ayurveda_dosha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ayurveda_dosha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/question_models/question_models.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Question {
+  final String id;
+  final String title;
+  final Map<String, bool> options;
+
+  Question({required this.id, required this.title, required this.options});
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Question(id: $id, title: $title, options: $options)';
+  }
+}
